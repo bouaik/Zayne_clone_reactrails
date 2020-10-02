76e760cbab09a5551079f546319abb0641a5a783
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-02 02:23:47 +0100

    Create Item componenet with content

---
+++ app/javascript/components/Table/Item.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Item = (props) => {
+    return (
+        <div className='row'>
+            <div className='col-md-10 offset-md-1'>
+                <div className='text-center'>
+                    <div className='card px-5'>
+                        <div class="row">
+                            <div className='col-md-4'>
+
+                            </div>
+                            <div className='col-md-8'>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Item
