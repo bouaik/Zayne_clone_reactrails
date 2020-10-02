dfe43d26d21a0a4f79f002ee5737925d12442107
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-02 02:46:26 +0100

    Create activeitem with content

--- app/javascript/components/Home/Jumbotron.js
+++ app/javascript/components/Home/Jumbotron.js
@@ -49,7 +49,7 @@ const Jumbotron = () => {
                         </div>
                         <div className='col col-sm-12 col-md-7'>
                             <div className='pt-4 mt-4 text-center'>
-                                <iframe width="560" height="315" src="https://www.youtube.com/embed/VQc613pd-2E" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/HrF8Op5MiTc" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                             </div>
                         </div>
                     </div>
--- app/javascript/components/Table/Thumbnail.js
+++ app/javascript/components/Table/Thumbnail.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Thumbnail = () => {
     return (
         <div className='py-4'>
-            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/VQc613pd-2E" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/HrF8Op5MiTc" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     )
 }
