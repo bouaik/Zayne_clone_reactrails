ceea8dee6a31563a539def706d0884714e0ffc69
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-02 03:35:52 +0100

    Clicked video get bigger works

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -10,10 +10,25 @@ class Home extends Component {
             course_modules: [
                 { id: 1, title: '1. Setup up new ruby on rails project', description: 'Lorem ipusm', active: false },
                 { id: 2, title: '2. Scaffold first medels', description: 'Lorem ipusm', active: false },
-                { id: 3, title: '3. Style views', description: 'Lorem ipusm', active: true },
+                { id: 3, title: '3. Style views', description: 'Lorem ipusm', active: false },
                 { id: 4, title: '4. Deploy to heroku', description: 'Lorem ipusm', active: false },
             ]
         }
+    }
+    handleVideoChange(item, e) {
+        e.preventDefault()
+
+        let course_modules = [...this.state.course_modules]
+
+        course_modules.map(data => {
+            data.active = false
+        })
+
+        item.active = !item.active
+
+        course_modules[item.id - 1] = item
+
+        this.setState({ course_modules })
     }
 
 
@@ -21,7 +36,7 @@ class Home extends Component {
         return (
             <div>
                 <Jumbotron />
-                <Table course_modules={this.state.course_modules} />
+                <Table handleVideoChange={this.handleVideoChange.bind(this)} course_modules={this.state.course_modules} />
             </div>
         )
     }
